5fe61b15ee2a601fda6ca5afedc099466882670e
devil-prog <krishmikaabisheka1@gmail.com>
2021-07-24 17:36:58 +0530

    Organized

--- main.py
+++ main.py
@@ -1,36 +1,42 @@
 ##########################ON WORKING##########################
+#with Gui coming soon
+import os
+
+class app:
+    def __init__(self,a,b,c):
+        self.a = float(a)
+        self.b = float(b)
+        self.c = float(c)    
+
+    def calculate(self, k):
+        i = self.b**2
+        i = i-4*self.a*self.c
+        i = i**0.5
+
+        x = -self.b+i
+        x = x/(2*self.a)
+        #or
+        y = -self.b-i
+        y = y/(2*self.a)
+
+        try:
+            print(f"{round(x,k)} or {round(y,k)}")
+        except:
+            print(f"{x} or {y}")
 
 def run():
-    import os
-
-    print('hello world!')
     a = float(input("Enter a: "))
     b = float(input("Enter b: "))
     c = float(input("Enter c: "))
-    k = int(input("How many decimal places you want: "))
 
-    #here you go>>>
-
-    i = b**2
-    i = i-4*a*c
-    i = i**0.5
-
-    x = -b+i
-    x = x/(2*a)
-    #or
-    y = -b-i
-    y = y/(2*a)
-
-    try:
-        print(f"{round(x,k)} or {round(y,k)}")
-    except:
-        print(f"{x} or {y}")
-        
-    os.system('pause')
-
+    a = app(a,b,c)
+    a.calculate(2)
+    
 for i in range(3):
     run()
+    os.system('pause')
 
 print("Dont be Lazy man")
+os.system('pause')
 
 #coded by Devil-prog
